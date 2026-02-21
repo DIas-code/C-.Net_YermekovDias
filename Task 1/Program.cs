f548d14e2172a013f8b95cca6360393c2f815b75
Yermekov Dias <diasermekov09@gmail.com>
2026-02-21 13:12:03 +0500

    Task 1

---
+++ Task 1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter name: ");
+string? name = Console.ReadLine();
+
+Console.Write("Enter age: ");
+int age = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter height: ");
+double height = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"Name: {name}  Age: {age}  Height: {height}m");
